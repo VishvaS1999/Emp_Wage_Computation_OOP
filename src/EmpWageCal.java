e8f68b9417ea2cac6017bf57aeb298220a66a4f2
Vishva <gvishva2601@gmail.com>
2022-11-04 22:08:31 +0530

    Total Working Hours Or Days For Month

--- src/EmpWageCal.java
+++ src/EmpWageCal.java
@@ -4,12 +4,15 @@ class EmpCalculation
     public static final int IS_FULLTIME = 1;
     public static final int IS_PARTTIME = 2;
     public static int empHrs;
+    public static int totalHrs = 0;
     public void empCheck()
     {
         int empCheck = (int)Math.floor(Math.random() * 10) % 3;
         switch(empCheck)
         {
             case IS_FULLTIME:
+
+
                 System.out.println("Employee is present and worked full-time");
                 empHrs = 8;
                 break;
@@ -22,6 +25,7 @@ class EmpCalculation
                 System.out.println("Employee is absent");
                 empHrs = 0;
         }
+        totalHrs = totalHrs + empHrs;
     }
 }
 class DailyWage
@@ -38,21 +42,26 @@ class DailyWage
     {
         totalWage = totalWage + totalDailyWage;
     }
+
 }
 public class EmpWageCal
 {
-    public static int WORKING_DAYS = 20;
+    public static int MAX_WORKING_DAYS = 20;
+    public static int MAX_HOURS = 100;
     public static void main(String[] args)
     {
-        for (int i = 1;  i<= WORKING_DAYS; i++)
+        int workingDays = 0;
+        while (workingDays < MAX_WORKING_DAYS  && EmpCalculation.totalHrs < MAX_HOURS)
         {
-            System.out.println("For DAY"+i);
+            workingDays++;
+            System.out.println("For DAY"+workingDays);
             EmpCalculation emp = new EmpCalculation();
             emp.empCheck();
             DailyWage wage = new DailyWage();
             wage.dailyWageCalculation();
             wage.totalWage();
         }
+        System.out.println("Employee's total working Hours = "+EmpCalculation.totalHrs);
         System.out.println("Total Wage of employee in a month = "+DailyWage.totalWage);
     }
 }
