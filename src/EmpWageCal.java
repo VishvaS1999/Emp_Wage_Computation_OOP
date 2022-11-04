02f07760f8e7fdab74aaca500895d595df1fe748
Vishva <gvishva2601@gmail.com>
2022-11-04 21:44:01 +0530

    Check Employee Attendance

--- src/EmpWageCal.java
+++ src/EmpWageCal.java
@@ -1,7 +1,27 @@
+import java.util.Random;
+class Attendance
+{
+    public static final int IS_FULL_TIME = 1;
+
+    public void empCheck()
+    {
+        int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+        {
+            System.out.println("Employee Is Present");
+        }
+        else
+        {
+            System.out.println("Employee Is Absent");
+        }
+    }
+}
 public class EmpWageCal
 {
     public static void main(String[] args)
     {
-        System.out.println("Welcome Employee Wage Computation Program");
+        Attendance attendance = new Attendance();
+        attendance.empCheck();
     }
 }
+
